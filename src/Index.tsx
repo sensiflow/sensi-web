ff2a368ab49c16dda469dbdcd775bcc0fea563de
TiagoJFil <tjgfil@gmail.com>
2023-03-13 19:32:42 +0000

    Added initial Project Structure

---
+++ src/Index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './app';
+
+const root = createRoot(
+  document.getElementById('root') 
+);
+
+root.render(
+    <App />
+);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    mode: "development",
+    resolve: {
+        extensions: [".js", ".ts", ".tsx"],
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'style-loader',
+                  'css-loader'
+                ]
+              }
+        ]
+    }
+}
