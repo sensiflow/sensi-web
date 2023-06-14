8f20180e9ab8680e91cfc140fa3e794f143c7e7a
costalapada <franciscocosta.py@gmail.com>
2023-06-14 04:20:24 +0100

    Changed device-checkbox-list to change color based on the theme mode

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -4,21 +4,28 @@ import { Copyright } from "./copyright";
 
 /**
  * An authentication form.
- * 
+ *
+ * TODO: Retrieve the actual value of the form fields on load instead of initializing them to empty strings
+ *
  * @param handleSubmit A function that handles the form submission
- * 
+ *
  * @returns A JSX element that can be used for login
  */
-export function SignInForm(
-  {handleSubmit}: {
-    handleSubmit: (e, email, password) => void,
-  }
-){
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
+export function SignInForm({
+  handleSubmit,
+}: {
+  handleSubmit: (e, email, password) => void;
+}) {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-    return(
-    <Box component="form" noValidate onSubmit={e => handleSubmit(e, email, password)} sx={{ mt: 1 }}>
+  return (
+    <Box
+      component="form"
+      noValidate
+      onSubmit={(e) => handleSubmit(e, email, password)}
+      sx={{ mt: 1 }}
+    >
       <TextField
         margin="normal"
         required
@@ -28,7 +35,9 @@ export function SignInForm(
         name="email"
         autoComplete="email"
         autoFocus
-        onChange={e=>{setEmail(e.target.value)}}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
       />
       <TextField
         margin="normal"
@@ -39,7 +48,9 @@ export function SignInForm(
         type="password"
         id="password"
         autoComplete="current-password"
-        onChange={e=>{setPassword(e.target.value)}}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
       />
       <Button
         type="submit"
@@ -50,19 +61,16 @@ export function SignInForm(
       >
         Sign in
       </Button>
-      <Grid 
+      <Grid
         container
         spacing={0}
         direction="column"
         alignItems="center"
         justifyContent="center"
       >
-        <Grid item>
-            {`Don't have an account? Contact your Administator`}
-        </Grid>
+        <Grid item>{`Don't have an account? Contact your Administator`}</Grid>
       </Grid>
       <Copyright sx={{ mt: 5 }} />
     </Box>
-  )
+  );
 }
-
--- src/components/group/devices-checkbox-list.tsx
+++ src/components/group/devices-checkbox-list.tsx
@@ -1,15 +1,14 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import { Checkbox, useTheme } from '@mui/material';
-import { debounce } from 'lodash';
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { Checkbox, useTheme } from "@mui/material";
+import { debounce } from "lodash";
 
-import { tokens } from '../../theme';
-import { Device } from '../../model/device';
-import { constants } from '../../constants';
-import { DeviceInformation } from '../../pages/dashboard_spa/group';
-
+import { tokens } from "../../theme";
+import { Device } from "../../model/device";
+import { constants } from "../../constants";
+import { DeviceInformation } from "../../pages/dashboard_spa/group";
 
 interface DevicesCheckboxListProps {
   devices: Array<DeviceInformation>;
@@ -22,26 +21,24 @@ interface DevicesCheckboxListProps {
   selectedDevicesHandler: (selectedDevices: Array<Device>) => void;
 }
 
-export default function DevicesCheckboxList(
-  { 
-    devices,
-    groupID,
-    isLoadingDevices,
-    resetScrollToTop,
-    handleSelectedDevices,
-    onScrollReset,
-    loadMoreDevices,
-    selectedDevicesHandler
-  }: DevicesCheckboxListProps 
-){
+export default function DevicesCheckboxList({
+  devices,
+  groupID,
+  isLoadingDevices,
+  resetScrollToTop,
+  handleSelectedDevices,
+  onScrollReset,
+  loadMoreDevices,
+  selectedDevicesHandler,
+}: DevicesCheckboxListProps) {
   const theme = useTheme();
-  const colors = tokens(theme.palette.mode)
+  const colors = tokens(theme.palette.mode);
   const [checked, setChecked] = React.useState<Array<Device>>([]);
-  const elementEvent = React.useRef(null)
+  const elementEvent = React.useRef(null);
 
   const getCheckedDeviceIndex = (device: Device) => {
-    return checked.findIndex((checkedDevice)=> device.id === checkedDevice.id)
-  }
+    return checked.findIndex((checkedDevice) => device.id === checkedDevice.id);
+  };
 
   const handleToggle = (value: Device) => () => {
     const currentIndex = getCheckedDeviceIndex(value);
@@ -57,79 +54,82 @@ export default function DevicesCheckboxList(
 
   //Set an event listener on the checkbox list to detect when the scroll reaches the bottom in order to load more devices.
   React.useEffect(() => {
-    const handleScroll = debounce(event => {
-        const isBottom = elementEvent.current?.clientHeight + event.target.scrollTop + 1 >= event.target.scrollHeight;
-        if(!isLoadingDevices && isBottom){
-          loadMoreDevices()
-        }
-    }, constants.devicesCheckBoxList.scrollDebounceTime)
-    
-    elementEvent.current?.addEventListener('scroll', handleScroll, true);
+    const handleScroll = debounce((event) => {
+      const isBottom =
+        elementEvent.current?.clientHeight + event.target.scrollTop + 1 >=
+        event.target.scrollHeight;
+      if (!isLoadingDevices && isBottom) {
+        loadMoreDevices();
+      }
+    }, constants.devicesCheckBoxList.scrollDebounceTime);
+
+    elementEvent.current?.addEventListener("scroll", handleScroll, true);
 
     return () => {
-       elementEvent.current?.removeEventListener('scroll', handleScroll);
-    }
-  }, [])
+      elementEvent.current?.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   //Resets the page to the top when the condition is met
   React.useEffect(() => {
-    if(elementEvent.current && resetScrollToTop){
-      elementEvent.current.scrollTop = 0
-      onScrollReset()
+    if (elementEvent.current && resetScrollToTop) {
+      elementEvent.current.scrollTop = 0;
+      onScrollReset();
     }
-  }, [resetScrollToTop])
+  }, [resetScrollToTop]);
 
   // Handles the selected devices when the condition is met
   React.useEffect(() => {
-    if(handleSelectedDevices){
-      selectedDevicesHandler(checked)
+    if (handleSelectedDevices) {
+      selectedDevicesHandler(checked);
     }
-  }, [handleSelectedDevices])
+  }, [handleSelectedDevices]);
 
   return (
     <List
       ref={elementEvent}
       sx={{
-        width: '100%',
+        width: "100%",
         maxWidth: constants.devicesCheckBoxList.maxWidth,
         minWidth: constants.devicesCheckBoxList.minWidth,
-        bgcolor: '#2f3748',
-        position: 'relative',
-        overflow: 'auto',
+        bgcolor: colors.primary[400],
+        position: "relative",
+        overflow: "auto",
         maxHeight: constants.devicesCheckBoxList.maxHeight,
         minHeight: constants.devicesCheckBoxList.minHeight,
         "&::-webkit-scrollbar": {
-          width: '0' //Hide scrollbar
-        }
+          width: "0", //Hide scrollbar
+        },
       }}
     >
       {devices.map((item) => (
-          <ListItem 
-            key={`item-${item.device.id}`}
-            secondaryAction={
-              <Checkbox
-                edge="end"
-                onChange={handleToggle(item.device)}
-                checked={getCheckedDeviceIndex(item.device) !== -1}
-                disabled={item.groupsID.includes(groupID)}
-                inputProps={{ 'aria-labelledby':  `checkbox-list-secondary-label-${item.device.id}` }}
-              />
-            }
-            sx={{
-              "& .MuiCheckbox-root.Mui-checked": {
-                color: colors.greenAccent[500],
-              },
-            }}
-          >
-            <ListItemText 
-              primary={item.device.name}
-              sx={{
-                marginRight: '250px'
+        <ListItem
+          key={`item-${item.device.id}`}
+          secondaryAction={
+            <Checkbox
+              edge="end"
+              onChange={handleToggle(item.device)}
+              checked={getCheckedDeviceIndex(item.device) !== -1}
+              disabled={item.groupsID.includes(groupID)}
+              inputProps={{
+                "aria-labelledby": `checkbox-list-secondary-label-${item.device.id}`,
               }}
             />
-          </ListItem>
-        ))}
+          }
+          sx={{
+            "& .MuiCheckbox-root.Mui-checked": {
+              color: colors.greenAccent[500],
+            },
+          }}
+        >
+          <ListItemText
+            primary={item.device.name}
+            sx={{
+              marginRight: "250px",
+            }}
+          />
+        </ListItem>
+      ))}
     </List>
   );
 }
-
